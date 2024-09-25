673d0a8d683ecaf0f18463f701d58794980eac2e
LuisAlvaradoRivas <lalvarado.rivas3134@gmail.com>
2024-09-25 14:31:02 -0700

    added timer

--- UltimateCowPig/Assets/Scripts/GameState/GameStateManager.cs
+++ UltimateCowPig/Assets/Scripts/GameState/GameStateManager.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class GameStateManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    private float timer;
+    public string timerText;
     void Start()
     {
-        
+        timer=60.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateTime();
+    }
+    private void updateTime(){
+        timer-=Time.deltaTime;
+        int minutes = Mathf.FloorToInt(timer/60);
+        int seconds = Mathf.FloorToInt(timer%60);
+        int milliseconds = Mathf.FloorToInt(timer*1000);
+        milliseconds=milliseconds%1000;
+
+        timerText= string.Format("{0:00}:{1:00}:{2:000}",minutes,seconds,milliseconds);
     }
 }
