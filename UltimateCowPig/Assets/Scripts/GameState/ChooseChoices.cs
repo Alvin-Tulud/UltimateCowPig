6d959b815e9fcebd6f068e8067451497e4588282
LuisAlvaradoRivas <lalvarado.rivas3134@gmail.com>
2024-09-25 13:16:32 -0700

    scripts for choosing

--- UltimateCowPig/Assets/Scripts/GameState/ChooseChoices.cs
+++ UltimateCowPig/Assets/Scripts/GameState/ChooseChoices.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ChooseChoices : MonoBehaviour
 {
+    public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,35 @@ public class ChooseChoices : MonoBehaviour
     void Update()
     {
         
+    }
+    void AddTime(){
+        //add time
+        //add barrier
+    }
+    void RemoveTime(){
+        //add time
+    }
+    void addSpeed(){
+         float currentBuff=Player.GetComponent<PlayerController>().GetSpeedBuff();
+            Player.GetComponent<PlayerController>().SetSpeedBuff(currentBuff+0.25f);
+    }
+    void addJump(){
+        Player.GetComponent<PlayerController>().jumpCount++;
+    }
+
+    //this one will choose a random buff to add 
+    void addBuff(){
+        Random rnd = new Random();
+        int rndBuff=rnd.Next(0,3); 
+        switch(rndBuff){
+            case 0:
+                addSpeed();
+                break;
+            case 1:
+                addJump();
+                break;
+        }
+        RemoveTime();
+        //remove time 
     }
 }
